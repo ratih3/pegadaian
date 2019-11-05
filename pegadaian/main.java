f71c6929b927514827ee571b16e5f82b20f73e1a
ratih3 <57377473+ratih3@users.noreply.github.com>
2019-11-05 10:41:44 +0700

    version3
    percobaan ke 3

--- pegadaian/main.java
+++ pegadaian/main.java
@@ -2,8 +2,8 @@ package belajargit;
 import java.io.*;
 public class main {
 public static void main(String[] args) {
-	System.out.println("Version 2");
-	System.out.println("hello world2");
+	System.out.println("Version 3");
+	System.out.println("hello world3");
 
 }
 }
