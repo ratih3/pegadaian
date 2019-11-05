2d753e0248151f81e30a82522ed1948524f17a9f
ratih3 <57377473+ratih3@users.noreply.github.com>
2019-11-05 09:58:02 +0700

    version1
    Percobaan versi 1

---
+++ pegadaian/main.java
@@ -0,0 +1,9 @@
+package belajargit;
+import java.io.*;
+public class main {
+public static void main(String[] args) {
+	System.out.println("Version 1");
+	System.out.println("hello world1");
+
+}
+}
